0b8e7c7c4bd403e5339b268efee594ab87552034
Tyler Pulse <tyler3321@gmail.com>
2017-10-10 15:00:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
-def fizzbuzz(int)
+def fizzbuzz(int, int2, int3)
   if int % 3 == 0
     "Fizz"
   end
 
-  if int % 5 == 0
+  if int2 % 5 == 0
     "Buzz"
   end
 
-  if int % 5 || 3 == 0
+  if int3 % 5 || 3 == 0
     "FizzBuzz"
   end
 
